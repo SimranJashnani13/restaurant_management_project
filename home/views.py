a1e5f8ea7a05e6c25abca350d98cd616e5ad705c
SimranJashnani13 <sinran.jashnani1234@gmail.com>
2025-08-23 18:51:04 +0530

    37350

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    restaurant_name = "Flavor Fiesta"
+    return render(request "home.html",{"restaurant_name":restaurant_name})
