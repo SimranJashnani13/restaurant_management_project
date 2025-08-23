66c03906de047a062282d9315a575f2ec30b9c1b
SimranJashnani13 <sinran.jashnani1234@gmail.com>
2025-08-23 19:13:49 +0530

    37351

--- home/views.py
+++ home/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def home(request):
     restaurant_name = "Flavor Fiesta"
     return render(request "home.html",{"restaurant_name":restaurant_name})
+
+def about(request):
+    restaurant_name = "Flavor Fiesta"
+    description = (
+        "Flavor Fiesta is a cozy restaurant serving freshly made dishes with love."
+        "We combine traditional dishes with modern flavours to give you dining experience worth remembering. "
+    )
+    return render(request "about.html",
+    {"restaurant_name":restaurant_name,
+     "description":description})
